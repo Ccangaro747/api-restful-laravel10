403c377fca9e1446e0ed595a451b6a6d8176686c
Camilo Cangaro <camilocangaro@gmail.com>
2024-04-11 09:27:50 -0300

    Eliminación de CORS cambios en el index.php

--- public/index.php
+++ public/index.php
@@ -1,15 +1,5 @@
 <?php
-
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-header("Allow: GET, POST, OPTIONS, PUT, DELETE");
-$method = $_SERVER['REQUEST_METHOD'];
-if($method == "OPTIONS") {
-    die();
-}
-
-
+//ELIMINE LOS CORS, POR EL ERROR QUE ME DABA, FUNCIONA SIN ENCABEZADO. VERIFICAR SI ES NECESARIO
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
